ffde34c85209aaa5a9152f05712296721078648d
Macelux <macelux@hotmail.com>
2020-08-01 19:36:38 +0100

    first commit

---
+++ src/components/TodoList.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="todo-item" v-bind:class="{'is-completed' : todo.completed}">
+        <p >
+            <input type="checkbox" v-on:change="markComplete">
+            {{todo.title}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "TodoItem",
+    props: ["todo"]
+}
+</script>
+
+<style>
+.todo-item {
+    background: #f4f4f4;
+    padding: 20px;
+    border-bottom: 1px #ccc dotted;
+}
+
+.is-completed {
+    text-decoration: line-through;
+}
+
+.del {
+    background: #ff0000;
+    color: #fff;
+    border: none;
+    padding: 5px 9px;
+    border-radius: 50%;
+    cursor: pointer;
+    float: right;
+}
+</style>
+
+
---
+++ src/components/Todos.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+       <div v-bind:key="todo.id" v-for="todo in todos">
+          <TodoList v-bind:todo = "todo"/>
+       </div>
+    </div>
+</template>
+
+<script>
+import TodoList from './TodoList';
+
+export default {
+    name: "Todos",
+    props: ["todos"],
+    components: {
+        TodoList
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
+
